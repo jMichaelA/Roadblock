881f3e2b8eede96ed0ed3a2dfce961452dd8d613
Jamahl Middleton <jamahlmiddleton@gmail.com>
2015-11-06 16:35:42 -0700

    Fixed the main menu

--- scripts/mainMenu.js
+++ scripts/mainMenu.js
@@ -20,107 +20,31 @@ MYGAME.menus['MainMenuState'] = (function (graphics,input, gameStack) {
 
         mouse = input.Mouse();
 
-/*
-        that.mouseOver = function (e) {
-
-            var canX = 0,
-                canY = 0,
-                gameArea = document.getElementById('gameArea');
-
-            canX = e.clientX;// + 75;
-            canY = e.clientY;// + 63;
-
-            //NEW GAME
-            tPos = newGameText.getPos();
-            tWidth = newGameText.getWidth();
-            tHeight = newGameText.getHeight();
-            if (canY > (tPos.y  - tHeight) && canY < (tPos.y + tHeight)) {
-                if (canX > tPos.x - (tWidth / 2) && canX < tPos.x + (tWidth / 2)) {
-                    graphics.
-                    mmSelected = true;
-                }
-                else {
-                    mmSelected = false;
-                    mmClicked = false;
-                }
-            }
-            else {
-                mmSelected = false;
-                mmClicked = false;
-            }
-
-            console.log("canvas X: " + canX + " canvasY: " +canY);
-            console.log("Element X: " + tPos.x + "Element Y: " + tPos.y);
-            //CONTROLS
-            tPos = controlsText.getPos();
-            tWidth = controlsText.getWidth();
-            tHeight = controlsText.getHeight();
-            if (canY > (tPos.y - tHeight) && canY < (tPos.y + tHeight)) {
-                if (canX > tPos.x - (tWidth / 2) && canX < tPos.x + (tWidth / 2)) {
-                    cSelected = true;
-                }
-                else {
-                    cSelected = false;
-                    cClicked = false;
-                }
-            }
-            else {
-                cSelected = false;
-                cClicked = false;
-            }
-            //CREDITS
-            tPos = creditsText.getPos();
-            tWidth = creditsText.getWidth();
-            tHeight = creditsText.getHeight();
-            if (canY > (tPos.y - tHeight) && canY < (tPos.y + tHeight)) {
-                if (canX > tPos.x - (tWidth / 2) && canX < tPos.x + (tWidth / 2)) {
-                    cdSelected = true;
-                }
-                else {
-                    cdSelected = false;
-                    cdClicked = false;
-                }
-            }
-            else {
-                cdSelected = false;
-                cdClicked = false;
-            }
-
-        };
-*/
-	/*
-        that.click = function (e) {
-            if (mmSelected) {
-                mmClicked = true;
-            }
-            if (cSelected) {
-                cClicked = true;
-            }
-            if (cdSelected) {
-                cdClicked = true;
-            }
-        };
-	*/
 
         that.initialize = function () {
-            //mouse.registerCommand('mousemove', that.mouseOver);
-            //mouse.registerCommand('mousedown', that.click);
-
         }
 
 
         that.update = function (elapsedTime) {
 
             var canvas = document.getElementById('id-canvas');
-
-			/*
-            newGameText.setPos(canvas.width - (canvas.width / 3), (canvas.height / 2));
-            controlsText.setPos(canvas.width - (canvas.width / 3), ((canvas.height / 2) + 150));
-            creditsText.setPos(canvas.width - (canvas.width / 3), ((canvas.height / 2) + 150 * 2));
-			*/
-			
+		
             mouse.update(elapsedTime);
             
+            $('#new_game').on('click',function(){
+                mmClicked = true;
+                $('#main_menu').hide();
+            });
+
+            $('#controls').on('click',function(){
+                cClicked = true;
+                $('#main_menu').hide();
+            });
+
+            $('#credits').on('click',function(){
+                cdClicked = true;
+                $('#main_menu').hide();
+            });
 
             //-----------------------------
             //  IF CLICKED
@@ -156,7 +80,6 @@ MYGAME.menus['MainMenuState'] = (function (graphics,input, gameStack) {
 
             //Draw main background
             graphics.background('media/mainMenuBg.png');
-            // graphics.background(null, 41.7418972, -111.8113987)
 
         };
 
