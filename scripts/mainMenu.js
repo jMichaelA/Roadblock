259d942eebbd381f38e81fccda647230353b292d
jMichaelA <jacob.m.adams@gmail.com>
2015-11-06 12:42:24 -0700

    added background

--- scripts/mainMenu.js
+++ scripts/mainMenu.js
@@ -256,7 +256,12 @@ MYGAME.menus['MainMenuState'] = (function (graphics,input, gameStack) {
             graphics.clear();
 
             //Draw main background
-            graphics.background('media/midTerm.png');
+            var google_tile = "http://maps.google.com/maps/api/staticmap?sensor=false&center=-34.397,150.644&zoom=8&size=300x400"
+            var imageObj = new Image();
+            imageObj.src = google_tile;
+
+            var map;
+            graphics.background(imageObj);
 
             //-------------------------------------------------------
             //  Draw Menu buttons
