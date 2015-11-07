0954c990d710d7c6e5e14d0228b33e224c687dcd
jMichaelA <jacob.m.adams@gmail.com>
2015-11-06 20:50:20 -0700

    fixed bug with map

--- scripts/gamePlay.js
+++ scripts/gamePlay.js
@@ -298,42 +298,4 @@ MYGAME.menus['GamePlayState'] = (function (graphics, input, gameStack) {
         Menu: Menu
     };
 
-
-
-function initiate_geolocation() {
-    navigator.geolocation.getCurrentPosition(handle_geolocation_query,handle_errors);
-}
-function handle_errors(error)
-{
-    switch(error.code)
-    {
-        case error.PERMISSION_DENIED: alert("user did not share geolocation data");
-        break;
-        case error.POSITION_UNAVAILABLE: alert("could not detect current position");
-        break;
-        case error.TIMEOUT: alert("retrieving position timed out");
-        break;
-        default: alert("unknown error");
-        break;
-    }
-}
-function handle_geolocation_query(position)
-{
-    var google_tile = "http://maps.google.com/maps/api/staticmap?sensor=false&center=" + position.coords.latitude + "," +
-                    position.coords.longitude + "&zoom=14&size=300x400&markers=color:blue|label:U|" +
-                    position.coords.latitude + ',' + position.coords.longitude;
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");   
-    var imageObj = new Image();
-    imageObj.src = google_tile;
-    changeBackground(imageObj);
-    // imageObj.onload = function(){
-    //   context.drawImage(imageObj, 0, 0);
-    // }
-}
-
-function changeBackground(image){
-    graphics.background(image);
-}
-
 }(MYGAME.graphics, MYGAME.input, MYGAME.gameStack));
